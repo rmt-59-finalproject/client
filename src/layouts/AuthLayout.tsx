448a332d9de1298379c306434f39660360d0b381
hafidzahm <hafizhahm123@gmail.com>
2025-05-01 19:14:02 +0700

    feat: adding guardLogin concept on authLayout

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router";
 
 export default function AuthLayout() {
+  useEffect(() => {
+    guardLogin();
+  }, []);
+  function guardLogin() {
+    const token = localStorage.getItem("Authorization");
+    if (!token) {
+      console.log("Token di localstorage tidak ada");
+    }
+  }
   return (
     <>
       <h1>---Navbar---</h1>
