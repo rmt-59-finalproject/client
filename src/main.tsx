4e749f7691fae367f8df4c9a6f8ca1270ad75a44
hafidzahm <hafizhahm123@gmail.com>
2025-05-01 19:05:10 +0700

    feat: adding authentication layout

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,19 @@ import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import AuthLayout from "./layouts/AuthLayout";
+import DashboardPage from "./pages/DashboardPage";
 
 createRoot(document.getElementById("root")!).render(
   // <StrictMode>
   <BrowserRouter>
     <Routes>
+      <Route element={<AuthLayout />}>
+        {/* desain layout dengan navbar + sesudah dapat auth */}
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Route>
+
+      {/* desain layout tanpa navbar + sebelum dapat auth */}
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
     </Routes>
