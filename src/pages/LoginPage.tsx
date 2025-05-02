0fe66dcab0a18242af34aedea33768b306360f7c
hafidzahm <hafizhahm123@gmail.com>
2025-05-02 08:59:04 +0700

    feat: adding login-card component

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,16 @@
 import { Button } from "@/components/ui/button";
 import { useEffect } from "react";
 import { useNavigate } from "react-router";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function LoginPage() {
   const navigate = useNavigate();
@@ -16,10 +26,51 @@ export default function LoginPage() {
 
     console.log("tidak ada token di localStorage");
   }
+
   return (
-    <div>
-      <h1>Hello Login</h1>
-      <Button>I'm button</Button>
+    <div className="flex flex-col justify-center items-center min-h-dvh w-full bg-amber-200">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Login ke Stockify</CardTitle>
+          <CardDescription>
+            Masukkan email untuk masuk ke akun kamu.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="******"
+                  required
+                />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+          <Button variant="neutral" className="w-full">
+            Login with Google
+          </Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
