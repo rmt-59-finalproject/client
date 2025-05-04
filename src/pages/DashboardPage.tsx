4932ed1f14b1a8c6c3ad467ad128a959a41d2ff2
hafidzahm <hafizhahm123@gmail.com>
2025-05-04 07:36:20 +0700

    refactor: refactoring landing paagae when nothing ahead

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -16,7 +16,7 @@ import { useNavigate } from "react-router";
 
 export default function DashboardPage() {
   const [role, useRole] = useState("driver");
-  const [usernameDriver, setUsernameDriver] = useState("dani_antar");
+  const [usernameDriver, setUsernameDriver] = useState("dani_antaraja");
   const [orderData, setOrderData] = useState<OrderType[]>([]);
   const navigate = useNavigate();
   useEffect(() => {
@@ -143,49 +143,61 @@ export default function DashboardPage() {
               </div>
             </div>
             <div className=" min-h-screen p-4 max-w-6xl font-bold text-lg w-full">
-              {orderData.map((order) => {
-                return (
-                  <Card key={order._id}>
-                    <CardContent>
-                      <div>
-                        <div className="flex flex-col justify-center items-center">
-                          <div className="flex flex-row justify-between items-ccnter w-full">
-                            <h2 className="text-lg font-semibold text-gray-900 mb-1">
-                              {order?._id} - {order?.outlet?.username}{" "}
-                            </h2>
-                            <Badge>{order?.status}</Badge>
-                          </div>
+              {orderData.length > 0 ? (
+                // ADA ORDERAN
+                <>
+                  {orderData.map((order) => {
+                    return (
+                      <Card key={order._id}>
+                        <CardContent>
+                          <div>
+                            <div className="flex flex-col justify-center items-center">
+                              <div className="flex flex-row justify-between items-ccnter w-full">
+                                <h2 className="text-lg font-semibold text-gray-900 mb-1">
+                                  {order?._id} - {order?.outlet?.username}{" "}
+                                </h2>
+                                <Badge>{order?.status}</Badge>
+                              </div>
 
-                          <p className="text-sm text-gray-500 mb-3">
-                            {order?.items?.length} items • Order created at{" "}
-                            {order?.createdAt}
-                          </p>
-                        </div>
-                        <div className="mb-4">
-                          <p className="text-sm font-medium text-gray-700 mb-2">
-                            Order Items:
-                          </p>
-                          <ul className="text-sm text-gray-800 space-y-1">
-                            {order?.items?.map((el) => {
-                              return (
-                                <div key={el?.productId}>
-                                  <li className="flex justify-between">
-                                    <span>{el?.productId}</span>
-                                    <span>{el?.quantity}</span>
-                                  </li>
-                                </div>
-                              );
-                            })}
-                          </ul>
-                        </div>
-                      </div>
-                    </CardContent>
-                    <CardFooter>
-                      <Button className="w-full">Verify Delivery</Button>
-                    </CardFooter>
-                  </Card>
-                );
-              })}
+                              <p className="text-sm text-gray-500 mb-3">
+                                {order?.items?.length} items • Order created at{" "}
+                                {order?.createdAt}
+                              </p>
+                            </div>
+                            <div className="mb-4">
+                              <p className="text-sm font-medium text-gray-700 mb-2">
+                                Order Items:
+                              </p>
+                              <ul className="text-sm text-gray-800 space-y-1">
+                                {order?.items?.map((el) => {
+                                  return (
+                                    <div key={el?.productId}>
+                                      <li className="flex justify-between">
+                                        <span>{el?.productId}</span>
+                                        <span>{el?.quantity}</span>
+                                      </li>
+                                    </div>
+                                  );
+                                })}
+                              </ul>
+                            </div>
+                          </div>
+                        </CardContent>
+                        <CardFooter>
+                          <Button className="w-full">Verify Delivery</Button>
+                        </CardFooter>
+                      </Card>
+                    );
+                  })}
+                </>
+              ) : (
+                // GAADA ORDERAN
+                <>
+                  <div className="min-h-20 opacity-70 flex justify-center items-center">
+                    <h1>Ga ada orderan nih, santuy dulu aja...</h1>
+                  </div>
+                </>
+              )}
             </div>
           </div>
         </>
