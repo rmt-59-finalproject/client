34adeb3260cc8c9f49142ea88bca6598a0ac127b
hafidzahm <hafizhahm123@gmail.com>
2025-05-04 18:04:33 +0700

    refactor: refactoring assign page warehouse

--- src/pages/AssignOrderPage.tsx
+++ src/pages/AssignOrderPage.tsx
@@ -1,13 +1,11 @@
 import { http } from "@/helpers/axios";
 import { OrderType } from "@/types";
 import { useEffect, useState } from "react";
-import TableHistoryOrder from "@/components/TableHistoryOrder";
 import {
   Select,
   SelectContent,
   SelectGroup,
   SelectItem,
-  SelectLabel,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
