b42f94f6c19d338319b1ef3999301ae3a5ae658c
hafidzahm <hafizhahm123@gmail.com>
2025-05-05 20:14:28 +0700

    wiring detail order

--- src/pages/warehouse/DetailOrderPage.tsx
+++ src/pages/warehouse/DetailOrderPage.tsx
@@ -2,13 +2,14 @@ import { BadgeStatusColor } from "@/components/BadgeStatusColor";
 import TableDetailOrder from "@/components/TableDetailOrder";
 import { Badge } from "@/components/ui/badge";
 import { http } from "@/helpers/axios";
+import { formatDate } from "@/lib/utils";
 import { OrderStatus, OrderType } from "@/types";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
 // deetail orderan diklik dari /dashboard atau /orders
 export default function DetailOrderPage() {
-  const [detail, setDetail] = useState<OrderType[]>([]);
+  const [detail, setDetail] = useState<OrderType>({});
   const params = useParams();
   const { orderId } = params;
   useEffect(() => {
@@ -17,9 +18,11 @@ export default function DetailOrderPage() {
 
   async function fetchDetail() {
     try {
-      const data = await http(`/orders?_id=${orderId}`);
-      console.log(data.data[0], "<-- fetchDetail");
-      const responseData: OrderType[] = data?.data;
+      const data = await http(`/orders/${orderId}`, {
+        withCredentials: true,
+      });
+      console.log(data.data, "<-- fetchDetail");
+      const responseData: OrderType = data?.data;
       setDetail(responseData);
     } catch (error) {
       console.log(error);
@@ -41,28 +44,30 @@ export default function DetailOrderPage() {
         <div className=" p-2.5 rounded-2xl border border-gray-400 bg-gray-300">
           <div className="px-5 pt-5">
             <h1>Order Id</h1>
-            <h1 className="text-lg font-bold">{detail[0]?._id}</h1>
+            <h1 className="text-lg font-bold">{detail?._id}</h1>
           </div>
           <div className="px-5 pt-5">
-            <h1>Outlet Owner</h1>
-            <h1 className="text-lg font-bold">{detail[0]?.outlet?.username}</h1>
+            <h1>Outlet Name</h1>
+            <h1 className="text-lg font-bold">{detail?.outlet?.name}</h1>
           </div>
           <div className="px-5 pt-5">
             <h1>Order assigned to driver:</h1>
-            <h1 className="text-lg font-bold">{detail[0]?.driver?.username}</h1>
+            <h1 className="text-lg font-bold">{detail?.driver?.name}</h1>
           </div>
           <div className="px-5 pt-5">
             <h1>Order Create Date</h1>
-            <h1 className="text-lg font-bold">{detail[0]?.createdAt}</h1>
+            <h1 className="text-lg font-bold">
+              {formatDate(detail?.createdAt)}
+            </h1>
           </div>
           <div className="px-5 pt-5">
             <h1>Status Order</h1>
-            <BadgeStatusColor status={detail[0]?.status as OrderStatus} />
+            <BadgeStatusColor status={detail?.status as OrderStatus} />
           </div>
           <div className="p-1">
             <h1 className="px-5 pt-5 pb-5">Order Items</h1>
             <div className="flex flex-col gap-2">
-              <TableDetailOrder data={detail[0]?.items || []} />
+              <TableDetailOrder data={detail?.items || []} />
             </div>
           </div>
         </div>
