2351131828e4a1a152cde7d31e22a876e4551b34
hafidzahm <hafizhahm123@gmail.com>
2025-05-07 20:50:07 +0700

    refactor: refactor card size

--- src/pages/warehouse/AssignOrderPage.tsx
+++ src/pages/warehouse/AssignOrderPage.tsx
@@ -118,184 +118,201 @@ export default function AssignOrderPage() {
     <div className="flex bg-[url('data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20viewBox=%270%200%2032%2032%27%20width=%2732%27%20height=%2732%27%20fill=%27none%27%20stroke=%27rgb(0%200%200%20/%200.2)%27%3e%3cpath%20d=%27M0%20.5H31.5V32%27/%3e%3c/svg%3e')] flex-col justify-start items-center min-h-screen w-full">
       <div className="w-full max-w-7xl flex flex-col">
         <div className="flex flex-row items-center px-5 pt-5 justify-between w-full ">
-          <div>
-            <h1 className="text-2xl font-bold">
-              Accept & Assign Incoming Order Requests
-            </h1>
-            <h1 className="text-lg opacity-50">
-              Lets accept & assign the order for continue the processes .
-            </h1>
+          <div className="flex flex-col w-full">
+            <div className="w-full flex flex-row justify-between items-center">
+              <div>
+                <h1 className="text-2xl font-bold">
+                  Accept & Assign Incoming Order Requests
+                </h1>
+                <h1 className="text-lg opacity-50">
+                  Lets accept & assign the order for continue the processes .
+                </h1>
+              </div>
+
+              <div className="p-5">
+                <Select onValueChange={(val) => setFilter(val)}>
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Requested" />
+                  </SelectTrigger>
+                  <SelectContent className="font-[family-name:Space_Mono]">
+                    <SelectGroup>
+                      <SelectItem value="requested">Requested</SelectItem>
+                      <SelectItem value="approved">Approved</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="p-5">
-          <Select onValueChange={(val) => setFilter(val)}>
-            <SelectTrigger className="w-[180px]">
-              <SelectValue placeholder="Requested" />
-            </SelectTrigger>
-            <SelectContent className="font-[family-name:Space_Mono]">
-              <SelectGroup>
-                <SelectItem value="requested">Requested</SelectItem>
-                <SelectItem value="approved">Approved</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </div>
-        <div className="p-5 flex flex-col justify-center items-center">
-          <div>
-            {filter === "requested" && (
+
+        <div className="p-5 flex flex-col w-full justify-center items-start">
+          <div className="w-full flex flex-col justify-center items-center">
+            {orderData.length > 0 ? (
               <>
-                <div className="flex flex-col gap-2 bg-blue-200 justify-center items-center">
-                  {orderData?.map((el) => {
-                    return (
-                      <Card key={el?._id}>
-                        <CardHeader>
-                          <div className="flex flex-row justify-between items-center">
-                            <h1>{el?.orderId}</h1>
-                            <BadgeStatusColor
-                              status={el?.status as OrderStatus}
-                            />
-                          </div>
-                        </CardHeader>
-                        <CardContent>
-                          <div className="flex flex-row justify-between items-center">
-                            <div>
-                              <h1 className="text-lg font-bold">Outlet</h1>
-                              <h1>{el?.outlet?.name}</h1>
-                            </div>
-                            <div>
-                              <h1 className="text-lg font-bold">
-                                Date Requested
-                              </h1>
-                              <h1>{formatDate(el?.createdAt)}</h1>
-                            </div>
-                          </div>
-                          <div className="p-5">
-                            <Table>
-                              <TableHeader>
-                                <TableRow>
-                                  <TableHead>Product Name</TableHead>
-                                  <TableHead>Quantity</TableHead>
-                                </TableRow>
-                              </TableHeader>
-                              <TableBody>
-                                {el?.items?.map((el) => {
-                                  return (
-                                    <TableRow key={el.name}>
-                                      <TableCell className="font-base">
-                                        {el.name}
-                                      </TableCell>
-                                      <TableCell>{el.quantity}</TableCell>
+                {" "}
+                {filter === "requested" && (
+                  <>
+                    <div className="flex flex-col w-full gap-2  justify-center items-center">
+                      {orderData?.map((el) => {
+                        return (
+                          <Card key={el?._id} className="w-6xl py-5">
+                            <CardHeader>
+                              <div className="flex flex-row justify-between items-center">
+                                <h1>{el?.orderId}</h1>
+                                <BadgeStatusColor
+                                  status={el?.status as OrderStatus}
+                                />
+                              </div>
+                            </CardHeader>
+                            <CardContent>
+                              <div className="min-w-full flex flex-row justify-between items-center">
+                                <div>
+                                  <h1 className="text-lg font-bold">Outlet</h1>
+                                  <h1>{el?.outlet?.name}</h1>
+                                </div>
+                                <div>
+                                  <h1 className="text-lg font-bold">
+                                    Date Requested
+                                  </h1>
+                                  <h1>{formatDate(el?.createdAt)}</h1>
+                                </div>
+                              </div>
+                              <div className="p-5">
+                                <Table>
+                                  <TableHeader>
+                                    <TableRow>
+                                      <TableHead>Product Name</TableHead>
+                                      <TableHead>Quantity</TableHead>
+                                      <TableHead>Unit</TableHead>
                                     </TableRow>
-                                  );
-                                })}
-                              </TableBody>
-                            </Table>
-                          </div>
-                        </CardContent>
-                        <CardFooter>
-                          <Button
-                            onClick={() => {
-                              submitRequest(el._id);
-                            }}
-                            className="w-full"
-                          >
-                            Accept Request Order
-                          </Button>
-                        </CardFooter>
-                      </Card>
-                    );
-                  })}
-                </div>
+                                  </TableHeader>
+                                  <TableBody>
+                                    {el?.items?.map((el) => {
+                                      return (
+                                        <TableRow key={el.name}>
+                                          <TableCell className="font-base">
+                                            {el.name}
+                                          </TableCell>
+                                          <TableCell>{el.quantity}</TableCell>
+                                          <TableCell>{el.unit}</TableCell>
+                                        </TableRow>
+                                      );
+                                    })}
+                                  </TableBody>
+                                </Table>
+                              </div>
+                            </CardContent>
+                            <CardFooter>
+                              <Button
+                                onClick={() => {
+                                  submitRequest(el._id);
+                                }}
+                                className="w-full"
+                              >
+                                Accept Request Order
+                              </Button>
+                            </CardFooter>
+                          </Card>
+                        );
+                      })}
+                    </div>
+                  </>
+                )}
+                {filter === "approved" && (
+                  <>
+                    <div className="flex flex-col gap-2 justify-center items-center w-full">
+                      {orderData?.map((el) => {
+                        return (
+                          <Card key={el?._id} className="w-6xl py-5">
+                            <CardHeader>
+                              <div className="flex flex-row w-full justify-between items-center">
+                                <h1>{el?.orderId}</h1>
+                                <BadgeStatusColor
+                                  status={el?.status as OrderStatus}
+                                />
+                              </div>
+                            </CardHeader>
+                            <CardContent>
+                              <div className="flex flex-row justify-between items-center">
+                                <div>
+                                  <h1 className="text-lg font-bold">Outlet</h1>
+                                  <h1>{el?.outlet?.name}</h1>
+                                </div>
+                                <div>
+                                  <h1 className="text-lg font-bold">
+                                    Date Requested
+                                  </h1>
+                                  <h1>{formatDate(el?.createdAt)}</h1>
+                                </div>
+                              </div>
+                              <div className="p-5">
+                                <h1>items</h1>
+                                <Table>
+                                  <TableHeader>
+                                    <TableRow>
+                                      <TableHead>Product</TableHead>
+                                      <TableHead>Quantity</TableHead>
+                                      <TableHead>Unit</TableHead>
+                                    </TableRow>
+                                  </TableHeader>
+                                  <TableBody>
+                                    {el?.items?.map((el) => {
+                                      return (
+                                        <TableRow key={el._id}>
+                                          <TableCell className="font-base">
+                                            {el.name}
+                                          </TableCell>
+                                          <TableCell>{el.quantity}</TableCell>
+                                          <TableCell>{el.unit}</TableCell>
+                                        </TableRow>
+                                      );
+                                    })}
+                                  </TableBody>
+                                </Table>
+                              </div>
+                            </CardContent>
+                            <CardFooter>
+                              <div className="flex flex-row w-full gap-2 justify-between items-center">
+                                <Select
+                                  onValueChange={(e) => setSelectedDrivers(e)}
+                                >
+                                  <SelectTrigger className="w-1/2">
+                                    <SelectValue placeholder="Select driver" />
+                                  </SelectTrigger>
+                                  <SelectContent className="font-[family-name:Space_Mono]">
+                                    <SelectGroup>
+                                      {driver.map((el) => {
+                                        return (
+                                          <SelectItem
+                                            key={el?._id}
+                                            value={el._id as string}
+                                          >
+                                            {el.name}
+                                          </SelectItem>
+                                        );
+                                      })}
+                                    </SelectGroup>
+                                  </SelectContent>
+                                </Select>
+                                <Button
+                                  className="w-1/2"
+                                  onClick={() => submitDriver(el?._id)}
+                                >
+                                  Assign Order to the Driver
+                                </Button>
+                              </div>
+                            </CardFooter>
+                          </Card>
+                        );
+                      })}
+                    </div>
+                  </>
+                )}
               </>
-            )}
-            {filter === "approved" && (
+            ) : (
               <>
-                <div className="flex flex-col gap-2 justify-center items-center max-w-5xl">
-                  {orderData?.map((el) => {
-                    return (
-                      <Card key={el?._id} className="w-full">
-                        <CardHeader>
-                          <div className="flex flex-row w-full justify-between items-center">
-                            <h1>{el?.orderId}</h1>
-                            <BadgeStatusColor
-                              status={el?.status as OrderStatus}
-                            />
-                          </div>
-                        </CardHeader>
-                        <CardContent>
-                          <div className="flex flex-row justify-between items-center">
-                            <div>
-                              <h1 className="text-lg font-bold">Outlet</h1>
-                              <h1>{el?.outlet?.name}</h1>
-                            </div>
-                            <div>
-                              <h1 className="text-lg font-bold">
-                                Date Requested
-                              </h1>
-                              <h1>{formatDate(el?.createdAt)}</h1>
-                            </div>
-                          </div>
-                          <div className="p-5">
-                            <h1>items</h1>
-                            <Table>
-                              <TableHeader>
-                                <TableRow>
-                                  <TableHead>Product</TableHead>
-                                  <TableHead>Quantity</TableHead>
-                                  <TableHead>Unit</TableHead>
-                                </TableRow>
-                              </TableHeader>
-                              <TableBody>
-                                {el?.items?.map((el) => {
-                                  return (
-                                    <TableRow key={el._id}>
-                                      <TableCell className="font-base">
-                                        {el.name}
-                                      </TableCell>
-                                      <TableCell>{el.quantity}</TableCell>
-                                      <TableCell>{el.unit}</TableCell>
-                                    </TableRow>
-                                  );
-                                })}
-                              </TableBody>
-                            </Table>
-                          </div>
-                        </CardContent>
-                        <CardFooter>
-                          <div className="flex flex-row w-full gap-2 justify-between items-center">
-                            <Select
-                              onValueChange={(e) => setSelectedDrivers(e)}
-                            >
-                              <SelectTrigger className="w-1/2">
-                                <SelectValue placeholder="Select driver" />
-                              </SelectTrigger>
-                              <SelectContent className="font-[family-name:Space_Mono]">
-                                <SelectGroup>
-                                  {driver.map((el) => {
-                                    return (
-                                      <SelectItem
-                                        key={el?._id}
-                                        value={el._id as string}
-                                      >
-                                        {el.name}
-                                      </SelectItem>
-                                    );
-                                  })}
-                                </SelectGroup>
-                              </SelectContent>
-                            </Select>
-                            <Button
-                              className="w-1/2"
-                              onClick={() => submitDriver(el?._id)}
-                            >
-                              Assign Order to the Driver
-                            </Button>
-                          </div>
-                        </CardFooter>
-                      </Card>
-                    );
-                  })}
-                </div>
+                <h1>Requested data is none</h1>
               </>
             )}
           </div>
